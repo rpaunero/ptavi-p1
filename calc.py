1cedc5ce86e60ecc4ecd1f5bfcd6b892bd7a73c0
raquel paunero herrero <rpaunero@zeta40.aulas.gsyc.es>
2015-09-28 15:48:30 +0200

    Terminado

--- calc.py
+++ calc.py
@@ -1,21 +1,39 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# Vamos a programar una calculadora
-
 import sys
 
-def main():
-    print("hola")
+def suma(operando1, operando2):
+	return operando1 + operando2
 
-def suma(sumando1, sumando2):
-    return sumando1 + sumando2
+def rest(operando1, operando2):
+	return operando1 - operando2
+
+def mult(operando1, operando2):
+	return operando1 * operando2
+
+def div(operando1, operando2):
+	return operando1 / operando2
 
 if __name__ == "__main__":
-#    print(sys.argv)
-    try:
-        operando1 = int(sys.argv[2])
-        operando2 = int(sys.argv[3])
-    except ValueError:
-        sys.exit("¡Tienen que ser enteros!")
-    print(suma(operando1, operando2))
+	try:
+		operando1 = int(sys.argv[1])
+		operador = sys.argv[2]
+		operando2 = int(sys.argv[3])
+	
+	except ValueError:
+		sys.exit("INTRODUCIR [NUMERO] [OPERADOR] [NUMERO]")
+
+	if sys.argv[2] == "suma":
+		print(suma(operando1, operando2))
+	elif sys.argv[2] == "resta":
+		print(rest(operando1, operando2))
+	elif sys.argv[2] == "mult":
+		print(mult(operando1, operando2))
+	elif sys.argv[2] == "div":
+			if operando2 == 0:
+				sys.exit("INDETERMINACIÓN")
+			else:
+				print(div(operando1, operando2))
+	else:
+		sys.exit("OPERADOR NO DEFINIDO")
